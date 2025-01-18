75c466c72a3a66eda792bda4e38d52ce075d3638
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2025-01-18 19:52:45 -0300

    added initial assets

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,11 @@
+import Header from "@/components/Header";
 import { ReactNode } from "react";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <main className="root-container">
       <div className="mx-auto max-w-7">
-        Header
+        <Header />
         <div className="mt-20 pb-20">{children}</div>
       </div>
     </main>
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,28 @@
+"use client";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 export default function Header() {
-  return <div>Header</div>;
+  const pathname = usePathname();
+
+  return (
+    <header className="my-10 flex justify-between gap-5">
+      <Link href="/">Bookwise</Link>
+
+      <ul className="flex flex-row items-center">
+        <li>
+          <Link
+            href="/library"
+            className={cn(
+              "text-base cursor-pointer capitalize",
+              pathname === "/library" ? "text-light-200" : "text-light-100"
+            )}
+          >
+            Library
+          </Link>
+        </li>
+      </ul>
+    </header>
+  );
 }
