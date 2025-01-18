a96d8c2f773d8ba0fc307482c138a3a005ce8c36
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2025-01-18 19:40:59 -0300

    initial layout code

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from "react";
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return (
+    <main className="root-container">
+      <div className="mx-auto max-w-7">
+        Header
+        <div className="mt-20 pb-20">{children}</div>
+      </div>
+    </main>
+  );
+}
