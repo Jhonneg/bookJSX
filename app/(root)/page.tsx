d46f5988601a18389ed98f7910ed3835d5b34353
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2025-02-02 21:04:20 -0300

    added types file

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,18 @@
 import BookList from "@/components/BookList";
 import BookOverview from "@/components/BookOverview";
 import { Button } from "@/components/ui/button";
+import { sampleBooks } from "@/constants";
 
 export default function Home() {
   return (
     <>
-      <BookOverview />
+      <BookOverview {...sampleBooks[0]} />
 
-      <BookList />
+      <BookList
+        title="latest Books"
+        books={sampleBooks}
+        containerClass="mt-28"
+      />
       <Button>Click me</Button>
     </>
   );
--- components/BookOverview.tsx
+++ components/BookOverview.tsx
@@ -1,8 +1,18 @@
-export default function BookOverview() {
+export default function BookOverview({
+  title,
+  author,
+  genre,
+  rating,
+  total_copies,
+  available_copies,
+  description,
+  color,
+  cover,
+}: Book) {
   return (
     <section className="book-overview">
       <div className="flex flex-1 flex-col gap-5">
-        <h1>Long book title</h1>
+        <h1>{title}</h1>
       </div>
     </section>
   );
