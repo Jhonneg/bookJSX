9a0ce257c1065d309b7649c6256dc8f25cf8b3ab
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2025-02-24 15:45:45 -0300

    auth layout markup done

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,5 +1,29 @@
+import Image from "next/image";
 import { ReactNode } from "react";
 
 export default function Layout({ children }: { children: ReactNode }) {
-  return <div>{children}</div>;
+  return (
+    <main className="auth-container">
+      <section className="auth-form">
+        <div className="auth-box">
+          <div className="flex flex-row gap-3">
+            <Image src="/icons/logo.svg" alt="logo" width={37} height={37} />
+            <h1 className="text-2xl font-semibold text-white">Bookwise</h1>
+          </div>
+
+          <div>{children}</div>
+        </div>
+      </section>
+
+      <section className="auth-illustration">
+        <Image
+          className="size-full object-cover"
+          src="/images/auth-illustration.png"
+          alt="auth"
+          height={1000}
+          width={1000}
+        />
+      </section>
+    </main>
+  );
 }
