c1c4955beb7ee24e5c3df85f870e243ed6bcd778
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2025-02-26 17:56:39 -0300

    created authform with zod schema

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,5 +1,17 @@
+import AuthForm from "@/components/AuthForm";
+import { signInSchema } from "@/lib/validations";
 import React from "react";
 
 export default function Page() {
-  return <div>signin</div>;
+  return (
+    <AuthForm
+      type="SIGN_IN"
+      schema={signInSchema}
+      defaultValues={{
+        email: "",
+        password: "",
+      }}
+      onSubmit={() => {}}
+    />
+  );
 }
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,3 +1,20 @@
+import AuthForm from "@/components/AuthForm";
+import { signInSchema, signUpSchema } from "@/lib/validations";
+import React from "react";
+
 export default function Page() {
-  return <div>signup</div>;
+  return (
+    <AuthForm
+      type="SIGN_UP"
+      schema={signUpSchema}
+      defaultValues={{
+        email: "",
+        password: "",
+        fullName: "",
+        universityId: 0,
+        universityCard: "",
+      }}
+      onSubmit={() => {}}
+    />
+  );
 }
