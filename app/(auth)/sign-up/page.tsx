bf09ed09452f5c140e11e7165a5c1bf834cf7216
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2025-02-26 17:58:21 -0300

    added client component directive

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,5 +1,7 @@
+"use client"
+
 import AuthForm from "@/components/AuthForm";
-import { signInSchema, signUpSchema } from "@/lib/validations";
+import { signUpSchema } from "@/lib/validations";
 import React from "react";
 
 export default function Page() {
